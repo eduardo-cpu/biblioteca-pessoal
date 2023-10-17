bcda3082d8378531cac97d7893c6197f351bef0c
Eduardo <eduardosantossm@hotmail.com>
2023-10-17 02:24:39 -0300

    adição do bootstrap por meio do laravel-mix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}} - Biblioteca Pessoal</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
 <h1>{{$title}}</h1>
